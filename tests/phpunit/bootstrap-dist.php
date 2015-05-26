e2350c5fdfd29ed6db83c85d8c92dcdd75d3f5c6
Michal Kalita <kalita.michal@gmail.com>
2015-05-26 16:49:08 +0200

    Renamed folder upload to www

--- tests/phpunit/bootstrap-dist.php
+++ tests/phpunit/bootstrap-dist.php
@@ -4,9 +4,9 @@ define('VERSION', '2.0');
 define('ADMIN_USERNAME', '');
 define('ADMIN_PASSWORD', '');
 
-define('CONFIG_ADMIN', __DIR__ . '/../../upload/admin/config.php');
-define('CONFIG_CATALOG', __DIR__ . '/../../upload/config.php');
-define('SQL_FILE', __DIR__ . '/../../upload/install/opencart.sql');
+define('CONFIG_ADMIN', __DIR__ . '/../../www/admin/config.php');
+define('CONFIG_CATALOG', __DIR__ . '/../../www/config.php');
+define('SQL_FILE', __DIR__ . '/../../www/install/opencart.sql');
 
 // Settings for Amazon Payments' Selenium tests
 define('AMAZON_PAYMENTS_SELLER_ID', '');
